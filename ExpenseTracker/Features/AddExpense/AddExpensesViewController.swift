50a6230dc7aa315499d50a40bdf0fdfd16f6a157
alicanov98 <alicanov98@gmail.com>
2026-08-04 18:53:49 +0400

    delete @available(*, unavailable)

--- ExpenseTracker/Features/AddExpense/AddExpensesViewController.swift
+++ ExpenseTracker/Features/AddExpense/AddExpensesViewController.swift
@@ -64,7 +64,6 @@ final class AddExpenseViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
 
-
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
